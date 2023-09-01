b70484e179f2f00538ab157d882b2fa3cd020774
Leonardo Matos <leomp120894@gmail.com>
2023-09-01 20:24:25 -0300

    Copying ShippingLine js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -25,7 +25,8 @@ module.exports = () => ({
       './js/PaymentMethods.js': path.resolve(__dirname, 'template/js/custom-js/components/PaymentMethods.js'),
       './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/components/APrices.html'),
       './js/APrices.js': path.resolve(__dirname, 'template/js/custom-js/components/APrices.js'),
-      './html/ShippingLine.html': path.resolve(__dirname, 'template/js/custom-js/components/ShippingLine.html')
+      './html/ShippingLine.html': path.resolve(__dirname, 'template/js/custom-js/components/ShippingLine.html'),
+      './js/ShippingLine.js': path.resolve(__dirname, 'template/js/custom-js/components/ShippingLine.js')
     }
   }
 })
