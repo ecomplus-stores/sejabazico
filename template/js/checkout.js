f72867070e0230d63861620fd1360e812893f2e5
matheusgnreis <matheus@e-com.club>
2024-01-12 16:50:06 -0300

    chore: set mailchimp as utm term

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -1,2 +1,13 @@
 import '#template/js/checkout'
 import './custom-js/checkout'
+
+
+const paramsURL = new URLSearchParams(window.location.search)
+const mcId = paramsURL.get('mc_cid')
+console.log('id mailchimp', mcId)
+if (mcId) {
+  const sessionUtm = JSON.parse(window.sessionStorage.getItem('ecomUtm') || '{}') 
+  sessionUtm.source = 'mailchimp'
+  sessionUtm.term = mcId
+  window.sessionStorage.setItem('ecomUtm', JSON.stringify(sessionUtm))
+}
--- template/js/index.js
+++ template/js/index.js
@@ -1,2 +1,14 @@
 import '#template/js/'
 import './custom-js/pages'
+
+
+const paramsURL = new URLSearchParams(window.location.search)
+const mcId = paramsURL.get('mc_cid')
+console.log('id mailchimp', mcId)
+if (mcId) {
+  const sessionUtm = JSON.parse(window.sessionStorage.getItem('ecomUtm') || '{}') 
+  sessionUtm.source = 'mailchimp'
+  sessionUtm.term = mcId
+  window.sessionStorage.setItem('ecomUtm', JSON.stringify(sessionUtm))
+}
+
